07a474ed62f66820d443447da948502529c3e942
Issam <ihannache@ferlab.bio>
2022-11-15 09:06:14 -0500

    Ajout du git tag avec le hash

--- ci/tasks/prepare-image.sh
+++ ci/tasks/prepare-image.sh
@@ -8,6 +8,8 @@ rm -fr ${ROOT_FOLDER}/output/ci && rm -fr ${ROOT_FOLDER}/output/.git
 
 pushd git-users-api
     VERSION=`echo "$(git rev-parse --short HEAD)-$(date +'%s')"`
+    VERSION-HASH=`echo "$(git rev-parse --short HEAD)`
 popd
 
 echo "${VERSION}" > ${ROOT_FOLDER}/output/version
+echo "${VERSION-HASH}" > ${ROOT_FOLDER}/output/version-hash
