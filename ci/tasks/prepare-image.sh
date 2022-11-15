426ebd8efdc12cf7ead28b1b1c1a688aad9fd2bf
Issam <ihannache@ferlab.bio>
2022-11-15 09:08:00 -0500

    Add missing colon

--- ci/tasks/prepare-image.sh
+++ ci/tasks/prepare-image.sh
@@ -8,7 +8,7 @@ rm -fr ${ROOT_FOLDER}/output/ci && rm -fr ${ROOT_FOLDER}/output/.git
 
 pushd git-users-api
     VERSION=`echo "$(git rev-parse --short HEAD)-$(date +'%s')"`
-    VERSION-HASH=`echo "$(git rev-parse --short HEAD)`
+    VERSION-HASH=`echo "$(git rev-parse --short HEAD)"`
 popd
 
 echo "${VERSION}" > ${ROOT_FOLDER}/output/version
