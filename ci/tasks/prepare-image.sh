93e2412457cc2b2e98c0adc4069488beed9f0678
Issam <ihannache@ferlab.bio>
2022-11-16 13:18:01 -0500

    remove has version from the task prepare

--- ci/tasks/prepare-image.sh
+++ ci/tasks/prepare-image.sh
@@ -8,8 +8,6 @@ rm -fr ${ROOT_FOLDER}/output/ci && rm -fr ${ROOT_FOLDER}/output/.git
 
 pushd git-users-api
     VERSION=`echo "$(git rev-parse --short HEAD)-$(date +'%s')"`
-    VERSION_HASH=`echo "$(git rev-parse --short HEAD)"`
 popd
 
 echo "${VERSION}" > ${ROOT_FOLDER}/output/version
-echo "${VERSION_HASH}" > ${ROOT_FOLDER}/output/version-hash
